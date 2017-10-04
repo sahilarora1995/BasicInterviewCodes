a36d9b4cf38eede1a5e9d88ecd12a9506e2f3981
sahilarora1995 <sarora160@gmail.com>
2017-10-04 11:40:48 -0700

    Add files via upload

---
+++ tree25- Topview of a binary tree.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int key;
+	Node *left;
+	Node *right;
+};
+
+Node * newNode(int key)
+{
+	Node*new_node=new Node;
+	new_node->key=key;
+	new_node->left=new_node->right=NULL;
+	return new_node;
+}
+
+
+ void verticaltraversal(Node *root)
+ {
+ 	if(!root) return ;
+ 	
+   map<int, vector<int> > m;
+   	int hd =0;
+ 	
+  queue<pair<Node*, int> > q;
+    q.push(make_pair(root, hd));
+ 
+ 	
+ 	while(!q.empty())
+    {
+    	pair<Node*, int> t= q.front();
+    	q.pop();
+    	hd= t.second;
+    	Node * node= t.first;
+    
+    	if(m.find(hd)==m.end())
+    	{
+    		cout<<node->key<<endl;
+    		m[hd].push_back(node->key);
+		}
+    	
+    	if(node->left) q.push(make_pair(node->left,hd-1));
+    	if(node->right) q.push(make_pair(node->right,hd+1));
+	}
+	
+
+ }
+
+int main()
+{
+
+  	 Node *root = newNode(51);
+    root->left = newNode(21);
+    root->right = newNode(13);
+    root->left->left = newNode(14);
+    root->left->right = newNode(65);
+    root->right->left = newNode(6);
+    root->right->right = newNode(67);
+    root->right->left->right = newNode(68);
+    root->right->right->right = newNode(96);
+    root->right->right->left= newNode(10);
+    root->right->right->left->right= newNode(161);
+    root->right->right->left->right->right= newNode(162);
+	verticaltraversal(root);
+	
+	return 0;
+}
