b54003bfc54eddd55e0fdc315adf2b17521b7a6b
sahilarora1995 <sarora160@gmail.com>
2019-06-17 04:46:57 -0700

    Rename array12- Next Greater number with same set of digits.cpp to array11- Next Greater number with same set of digits.cpp

--- Arrays/array11- Next Greater number with same set of digits.cpp
+++ Arrays/array11- Next Greater number with same set of digits.cpp
@@ -2,22 +2,22 @@
 Problem Statement:
 -----------------------
 Given a number n, find the smallest number that has same set of digits as n and is greater than 
-n. If n is the greatest possible number with its set of digits, then print “not possible”.
+n. If n is the greatest possible number with its set of digits, then print â€œnot possibleâ€.
 
 Core Logic:
 ---------------------
 Following is the algorithm for finding the next greater number.
 I) Traverse the given number from rightmost digit, keep traversing till you find a digit which is 
-smaller than the previously traversed digit. For example, if the input number is “534976”, we stop 
-at 4 because 4 is smaller than next digit 9. If we do not find such a digit, then output is “Not Possible”.
+smaller than the previously traversed digit. For example, if the input number is â€œ534976â€, we stop 
+at 4 because 4 is smaller than next digit 9. If we do not find such a digit, then output is â€œNot Possibleâ€.
 
-II) Now search the right side of above found digit ‘d’ for the smallest digit greater than ‘d’. For “534976?, 
-the right side of 4 contains “976”. The smallest digit greater than 4 is 6.
+II) Now search the right side of above found digit â€˜dâ€™ for the smallest digit greater than â€˜dâ€™. For â€œ534976?, 
+the right side of 4 contains â€œ976â€. The smallest digit greater than 4 is 6.
 
 III) Swap the above found two digits, we get 536974 in above example.
 
-IV) Now sort all digits from position next to ‘d’ to the end of number. The number that we get after sorting 
-is the output. For above example, we sort digits in bold 536974. We get “536479” which is the next greater 
+IV) Now sort all digits from position next to â€˜dâ€™ to the end of number. The number that we get after sorting 
+is the output. For above example, we sort digits in bold 536974. We get â€œ536479â€ which is the next greater 
 number for input 534976.
 
 Time Complexity:
