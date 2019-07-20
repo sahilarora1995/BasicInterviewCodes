084206d5032caa02a607e25cb6f44a389c34830c
sahilarora1995 <sarora160@gmail.com>
2019-07-20 13:22:22 +0530

    Add files via upload
    Matching Parenthesis
    ------------------------
    
    ** Assignment question at Mtech-CSE at IIITB***
    
    
    Write a Java program to read in a file containing a sequence of parentheses of various kinds -
    (){}[] - in any order and combination, and check if the parentheses are “balanced”.
    In the case of failure, the program should specify the position in the input stream where the first
    mismatch is detected (the first input character is at location 0)
    For example, for the input,
    ([]{()})
    the output should be success
    and for the input
    ([]{())}
    the output should be failed at 6
    Implement this without using explicit arrays/lists/stacks or other heap memory.

---
+++ Strings/MatchingParanwithoutStack.java
@@ -0,0 +1,89 @@
+
+	// Java program to check if parenthesis are 
+	// balanced or not in an expression. 
+	import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays; 
+
+	class MatchingParanwithoutStack 
+	{ 
+
+	static char findClosing(char c) 
+	{ 
+		if (c == '(') 
+			return ')'; 
+		if (c == '{') 
+			return '}'; 
+		if (c == '[') 
+			return ']'; 
+		return Character.MIN_VALUE; 
+	} 
+
+	// function to check if parenthesis are 
+	// balanced. 
+	static boolean check(char expr[], int n) 
+	{ 
+		// Base cases 
+		if (n == 0) 
+			return true; 
+		if (n == 1) 
+			return false; 
+		if (expr[0] == ')' || expr[0] == '}' || expr[0] == ']') 
+			return false; 
+
+		// Search for closing bracket for first 
+		// opening bracket. 
+		char closing = findClosing(expr[0]); 
+
+		// count is used to handle cases like 
+		// "((()))". We basically need to 
+		// consider matching closing bracket. 
+		int i, count = 0; 
+		for (i = 1; i < n; i++) 
+		{ 
+			if (expr[i] == expr[0]) 
+				count++; 
+			if (expr[i] == closing) 
+			{ 
+				if (count == 0) 
+					break; 
+				count--; 
+			} 
+		} 
+
+		// If we did not find a closing 
+		// bracket 
+		if (i == n) 
+			return false; 
+
+		// If closing bracket was next 
+		// to open 
+		if (i == 1) 
+			return check(Arrays.copyOfRange(expr, n-2, n), n - 2); 
+		// If closing bracket was somewhere 
+		// in middle. 
+		return check(Arrays.copyOfRange(expr, 1, n), i - 1) && 
+			check(Arrays.copyOfRange(expr,(i+1),n), n - i - 1); 
+	} 
+
+	// Driver code 
+	public static void main(String[] args) throws IOException 
+	{ 
+		File file = new File("C:\\Users\\Sahil Arora\\eclipse-workspace\\MuraliSir\\src\\string"); 
+		  
+		 BufferedReader br = new BufferedReader(new FileReader(file)); 
+		  char expr[]= br.readLine().toCharArray();
+	//	char expr[] = "[(])".toCharArray(); 
+		int n = expr.length; 
+		if (check(expr, n)) 
+			System.out.println("Balanced"); 
+		else
+			System.out.println("Not Balanced"); 
+	} 
+	} 
+
+	/* This code contributed by PrinciRaj1992 */
+
