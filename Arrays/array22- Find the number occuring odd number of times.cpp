d503b4ed8416d0900d5a3540cb7f3a3e37ca0cb6
sahilarora1995 <sarora160@gmail.com>
2019-07-27 19:12:41 +0530

    Add files via upload

---
+++ Arrays/array22- Find the number occuring odd number of times.cpp
@@ -0,0 +1,50 @@
+/*
+Author: Sahil Arora
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<bits/stdc++.h>
+#define ll int
+#define debug(x) cout << x<< " ";
+#define pii pair<int,int>
+#define F first
+#define S second
+#define WL(i,s) while(i<s.length())
+#define MAX INT_MAX
+#define FL(i) for(i=0;i<n;i++)
+#define MID(l,h) (l+(h-l))/2
+#define NODE struct node*
+#define START signed main()
+#define STACK stack<node*> 
+#define TR temp->right
+#define TL temp->left
+#define SIZE(a) sizeof(a)/sizeof(a[0])
+#define CH char
+#define IO1  ios::sync_with_stdio(0);
+#define IO2    cin.tie(0);
+#define IO3 	cout.tie(0);
+using namespace std;
+
+
+void oddOccur(ll a[],ll n)
+{
+	unordered_map<int, int> m;
+	for(ll i=0;i<n;i++)
+	{
+		m[a[i]]++;
+	}
+	for(auto itr: m)
+	{
+		if(itr.second%2 !=0)
+		debug(itr.first);
+	}
+}
+START
+{
+	IO1 IO2 IO3;
+	ll a[]= {5,7,2,7,5,2,5};
+	ll n=SIZE(a);
+	oddOccur(a,n);
+	return 0;
+}
